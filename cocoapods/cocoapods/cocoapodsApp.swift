068dec6e5acf3278794eee9a935d3ee84c9123c9
Daniel Alpizar <alpizar0610@gmail.com>
2021-09-18 11:13:32 -0600

    Add files via upload

---
+++ cocoapods/cocoapods/cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoapodsApp.swift
+//  cocoapods
+//
+//  Created by Daniel Alpizar on 18/9/21.
+//
+
+import SwiftUI
+
+@main
+struct cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
